9eb7dbfd63c39413cd0a8bdb14ed571cdc56d016
Ujjwal Acharya <81429137+ujjwalshiva@users.noreply.github.com>
2022-12-22 21:58:44 +0530

    GDSC IARE Revamp

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.onkeydown = function (e) {
+if (localStorage.getItem('name') && localStorage.getItem('email')) {
+    document.getElementById('name').value = localStorage.getItem('name');
+    document.getElementById('email').value = localStorage.getItem('email');
+}
+
+
+document.onkeydown = function(e) {
     if (event.keyCode == 123) {
         return false;
     }
@@ -18,7 +24,7 @@ document.onkeydown = function (e) {
 
 document.getElementById("submit").addEventListener("click", submit, false);
 
-console.log("Anna Namaste");
+console.log("Loading Modules...");
 
 var name;
 var email;
@@ -51,8 +57,8 @@ const sem_3 = {
 }
 
 const sem_4 = {
-    "Business Economics and Financial Analysis": "https://drive.google.com/drive/folders/19Wh2kGLc6YiMnOHIUJ3Z9atDFJZvmoKr?usp=sharing",
-    "Design and Analysis of Algorihtms": "https://drive.google.com/drive/folders/1URRkH1a1hDHW8f6Zrl7tM-O2MHeLae3b?usp=sharing",
+    "Business Economics & Financial Analysis": "https://drive.google.com/drive/folders/19Wh2kGLc6YiMnOHIUJ3Z9atDFJZvmoKr?usp=sharing",
+    "Design & Analysis of Algorihtms": "https://drive.google.com/drive/folders/1URRkH1a1hDHW8f6Zrl7tM-O2MHeLae3b?usp=sharing",
     "Database Management Systems": "https://drive.google.com/drive/folders/1fTRGvZmBkayzFd3q7FjVO5RK33pG1eDu?usp=sharing",
     "Operating Systems": "https://drive.google.com/drive/folders/16PVWMEYCQassfaPBjgsrupRkY5eb46P3?usp=sharing",
     "Theory Of Computation": "https://drive.google.com/drive/folders/1nvbnNLo3G2FIoqN9k_e3mtlvdKvFOq_C?usp=sharing",
@@ -70,7 +76,7 @@ const sem_6 = {
 function sub_sem() {
     semester = document.querySelector('#semester').value;
     let html = "";
-    for (let i = 0; i<Object.keys(eval(semester)).length;i++) {
+    for (let i = 0; i < Object.keys(eval(semester)).length; i++) {
         line = `<option value="${Object.keys(eval(semester))[i]}">${Object.keys(eval(semester))[i]}</option>`;
         html += line;
     };
@@ -103,41 +109,107 @@ function submit() {
             selected.push(option.value);
         }
     };
-    for (i=0;i<6;i++) {
+    for (i = 0; i < 6; i++) {
         selected.push('');
     }
 
 
-    if (selected[0]=='' || name=='' || email=="" || semester=="") {
-        return alert("Missing Field. Please fill all the details!");
+    if (selected[0] == '' || name == '' || email == "" || semester == "") {
+        return Toastify({
+            text: "Please enter all the details and then submit",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            offset: {
+                x: 0, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+                y: 75 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+            },
+            style: {
+                background: "#EA4335",
+                color: "white"
+            },
+        }).showToast();
     };
 
     if (document.getElementById("agree").checked == false) {
-        return alert("Please confirm by marking the checkbox!");
+        return Toastify({
+            text: "Please confirm by marking the checkbox!",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            offset: {
+                x: 0, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+                y: 75 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+            },
+            style: {
+                background: "#EED202",
+                color: "white"
+            },
+        }).showToast();
     };
-    
-    if (validate_mail(email)!== true) {
-        return alert("Incorrect Mail Address. Please use Domain Mail Address only.")
+
+    if (validate_mail(email) !== true) {
+        return Toastify({
+            text: "🚨 Incorrect Mail Address. Please use Domain Mail Address only",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            offset: {
+                x: 0, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+                y: 75 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+            },
+            style: {
+                background: "#EED202",
+                color: "white"
+            },
+        }).showToast();
     };
+
+    Toastify({
+        text: "🎉 Your file will be downloaded now",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        offset: {
+            x: 0, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+            y: 75 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+        style: {
+            background: "#34A853",
+            color: "white"
+        },
+    }).showToast()
 
     console.log(name);
     console.log(email);
     console.log(semester);
     console.log(selected);
 
+    localStorage.setItem("name", name);
+
+    localStorage.setItem("email", email);
+
+
     var total = '';
-    for (i=0;i<selected.length;i++) {
+    for (i = 0; i < selected.length; i++) {
         if (selected[i] != '') {
             if (i == 0) {
                 total = total + selected[i];
-            }
-            else {
+            } else {
                 total = total + ', ' + selected[i];
             }
-            
+
         }
     }
-    
+
     make_pdf(name, subjects, selected, semester, sem_1, sem_2, sem_3, sem_4, sem_5, sem_6);
     const options = {
         method: 'POST',
@@ -153,7 +225,4 @@ function submit() {
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
-    
 }
-
-
